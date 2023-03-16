b57f9fbee5ea2910a2e7ac1aa4e3e739d6ab39e3
nimosarang <111734043+nimosarang@users.noreply.github.com>
2023-03-17 02:13:55 +0900

    first commit

---
+++ src/main/java/com/squarecross/photoalbum/domain/Album.java
@@ -0,0 +1,34 @@
+package com.squarecross.photoalbum.domain;
+
+import java.util.Date;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.persistence.UniqueConstraint;
+import lombok.Data;
+import org.hibernate.annotations.CreationTimestamp;
+
+@Entity
+@Data
+@Table(name = "album", schema = "photo_album", uniqueConstraints = {
+    @UniqueConstraint(columnNames = "album_id")})
+public class Album {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) //가장 최근 id에 +1 을 해서 다음 아이디를 생성합니다.
+    @Column(name = "album_id", unique = true, nullable = false)
+    private Long albumId;
+
+    @Column(name = "album_name", unique = true, nullable = false)
+    private String name;
+
+    @Column(name = "created_at", unique = true, nullable = false)
+    @CreationTimestamp //새로운 앨범을 생성해 DB INSERT 할 때 자동으로 현재 시간을 입력해줍니다.
+    private Date createdAt;
+
+    public Album() {
+    }
+}
